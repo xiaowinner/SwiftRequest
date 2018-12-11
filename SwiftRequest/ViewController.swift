7145bc3c4e4770e0b69d06bd5cff52aee15cddfa
原晓文 <yuanxiao225@icloud.com>
2018-12-11 10:22:25 +0800

    Initial Commit

---
+++ SwiftRequest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftRequest
+//
+//  Created by 原晓文 on 2018/12/11.
+//  Copyright © 2018 xiaowinner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
